f3eca6403ed006a219ec2436e3d5ff97a3d7b372
Stilram19 <bednaouiomar19@gmail.com>
2026-06-28 17:42:20 +0100

    level4

---
+++ level4/source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int m = 0;
+
+int p(char *param_1) {
+  printf(param_1);
+  return (0);
+}
+
+int n(void) {
+  char buffer[520];
+ 
+  fgets(buffer,0x200,stdin);
+  p(buffer);
+  if (m == 0x1025544) {
+    system("/bin/cat /home/user/level5/.pass");
+  }
+  return (0);
+}
+
+int main(void) {
+  n();
+  return (0);
+}
